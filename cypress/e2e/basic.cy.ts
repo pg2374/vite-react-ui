6e7ec0216b6b0808d2a534afa114d4bfc37630ab
piyushgautam05 <piyush@kanlastech.com>
2025-04-03 02:21:10 -0400

    test cypress failure message
    Signed-off-by: piyushgautam05 <piyush@kanlastech.com>

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -2,7 +2,7 @@ describe("Basic App Tests", () => {
   it("loads the homepage", () => {
     cy.visit("/");
     cy.contains("My todos").should("exist");
-    cy.get("button").contains("new").should("exist");
+    cy.get("button3").contains("new").should("exist");
     cy.contains("App successfully hosted").should("exist");
   });
 });
